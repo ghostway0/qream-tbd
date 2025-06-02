e212be7342c8a4dc5742c4604594662503a9a2eb
ghostway <ghostway@tuta.io>
2025-06-02 21:04:23 +0300

    utility reformat

--- src/arm64.cpp
+++ src/arm64.cpp
@@ -4,7 +4,9 @@
 #include "qream/ir.h"
 #include "qream/match.h"
 
-inline void emit32(uint32_t instr, auto &out) {
+inline void emit_3reg(uint32_t opcode21, uint32_t rd, uint32_t rn, uint32_t rm, auto &out) {
+  // [31:21] opcode [20:16] Rm [15:10] unused [9:5] Rn [4:0] Rd
+  uint32_t instr = (opcode21 << 21) | (rm << 16) | (rn << 5) | rd;
   out++ = instr & 0xFF;
   out++ = (instr >> 8) & 0xFF;
   out++ = (instr >> 16) & 0xFF;
